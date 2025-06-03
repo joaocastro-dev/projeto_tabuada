dde57b8a993e1c2e3a80131783c9e45cdd4a3f51
joaocastro-dev <joaocastro12629@gmail.com>
2025-06-03 14:50:37 -0300

    projeto tabuada

---
+++ src/Tabuada.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Tabuada {
+    public static void main(String[] args) {
+
+        int numero=2;
+
+
+        for(int i=1;numero<=10;numero++){
+            System.out.println(numero + " x " + i + " = " + (numero * i));        }
+
+    }
+}
